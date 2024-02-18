b0844bbf4709c36d437d0850a1f796050a563c97
Joe Devietti <joe@devietti.net>
2024-02-17 22:16:37 -0500

    move auipc test to HW3B, for reals this time

--- hw3-singlecycle/testbench.py
+++ hw3-singlecycle/testbench.py
@@ -406,7 +406,7 @@ RV_TEST_BINARIES = [
 
 rvTestFactory = TestFactory(test_function=riscvTest)
 if 'RVTEST_ALUBR' in os.environ:
-    RV_TEST_BINARIES = RV_TEST_BINARIES[:28]
+    RV_TEST_BINARIES = RV_TEST_BINARIES[:27]
     pass
 rvTestFactory.add_option(name='binaryPath', optionlist=RV_TEST_BINARIES)
 rvTestFactory.generate_tests()
