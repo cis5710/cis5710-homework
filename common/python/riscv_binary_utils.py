574401a6a48e267c8b148e905ba128f32b0eeca5
Joe Devietti <devietti@cis.upenn.edu>
2025-01-29 01:15:35 -0500

    add riscv_binary_utils.loadBinaryIntoHexFile()

--- common/python/riscv_binary_utils.py
+++ common/python/riscv_binary_utils.py
@@ -20,7 +20,7 @@ assert shutil.which(ASSEMBLER) is not None, f"Couldn't find assembler program {A
 assert shutil.which(READELF) is not None, f"Couldn't find readelf program {READELF}"
 
 def asm(dut, assemblyCode):
-    """Assembles the given RISC-V code, returning the machine code as a list of numbers"""
+    """Assembles the given RISC-V code, and loads it into memory via cocotb"""
 
     # avoid assembler warning about missing trailing newline
     if not assemblyCode.endswith('\n'):
@@ -58,6 +58,42 @@ def loadBinaryIntoMemory(dut, binaryPath):
         for i in range(len(words)):
             # NB: doesn't work if we try to pass dut.memory.mem_array as an argument, need top-level dut
             dut.memory.mem_array[memBaseAddr + i].value = words[i]
+            pass
+        pass
+    pass
+
+def loadBinaryIntoHexFile(binaryPath, hexfilePath, maxWords=0):
+    """Read the given binary's sections, and write them out to a file for $readmemh"""
+    
+    sectionInfo = getSectionInfo(binaryPath)
+    # print(sectionInfo)
+    sectionsToLoad = ['.start', '.text', '.rodata', '.eh_frame']
+
+    with open(hexfilePath,'w') as fd:
+        micByteOffset = 0
+
+        for sectionName in sectionsToLoad:
+            if sectionName not in sectionInfo:
+                continue
+            offset = sectionInfo[sectionName]['offset']
+            length = sectionInfo[sectionName]['size']
+            words = extractDataFromBinary(binaryPath, offset, length + (length % 4))
+            # if sectionName == '.rodata':
+            #     print(['0x%x' % w for w in words])
+            #     pass
+            memBaseAddr = sectionInfo[sectionName]['address']
+            if memBaseAddr + len(words) > maxWords:
+                print(f"code reaches address {memBaseAddr + len(words)} but we can only handle up to {maxWords}")
+                sys.exit(1)
+
+            print(f"loading {sectionName} section ({len(words)} words) into memory starting at 0x{memBaseAddr:x}")
+            if memBaseAddr > 0 and memBaseAddr != micByteOffset:
+                fd.write(f'@{memBaseAddr:x}\n')
+                pass
+            for i in range(len(words)):
+                fd.write(f'{words[i]:08x}\n')
+                micByteOffset += 4
+                pass
             pass
         pass
     pass
