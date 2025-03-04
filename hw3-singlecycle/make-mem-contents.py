308846636d51bc6e3d7476b17f6480fce15df6ac
Joe Devietti <devietti@cis.upenn.edu>
2025-03-03 23:39:01 -0500

    add C version of ledrop game HW3 demo

--- hw3-singlecycle/make-mem-contents.py
+++ hw3-singlecycle/make-mem-contents.py
@@ -6,7 +6,8 @@ sys.path.append(str(p))
 import riscv_binary_utils
 
 riscv_binary_utils.loadBinaryIntoHexFile(
-    'ledrop/target/riscv32im-unknown-none-elf/release/ledrop',
+    #'ledrop-rust/target/riscv32im-unknown-none-elf/release/ledrop', # Rust version
+    'ledrop-c/ledrop.bin', # C version
     'mem_initial_contents.hex',
     maxWords=1024
 )
