81f3c45025f66ed50d56aa3cb6ff3143123ab5f9
salzhang <salzhang84@gmail.com>
2025-03-13 16:13:27 -0400

    establish a basic echo between the pc and the softcore

--- hw5-pipelined/system/System.sv
+++ hw5-pipelined/system/System.sv
@@ -2,6 +2,7 @@
 `include "DatapathPipelined.sv"
 `include "txuartlite.v"
 `include "rxuartlite.v"
+`include "rx2cpu_bridge.sv"
 
 module SystemResourceCheck (
     input wire external_clk_25MHz,
@@ -14,7 +15,7 @@ module SystemResourceCheck (
     .input_clk_25MHz(external_clk_25MHz),
     .clk_proc(clk_proc),
     .locked(clk_locked)
-    );
+  );
 
   wire [31:0] pc_to_imem, insn_from_imem, mem_data_addr, mem_data_loaded_value, mem_data_to_write;
   wire [3:0] mem_data_we;
@@ -60,18 +61,38 @@ module SystemDemo(
   output [7:0] led,
   output ftdi_rxd,
   output wifi_gpio0);
+  
+  //CPU & MEMORY
+  localparam int MmapOutput   = 32'hFF00_1000;
+  localparam int MmapInput    = 32'hFF00_2000;
 
-  localparam MAX_INPUT_WIDTH = 64;
+  wire clk_proc, clk_locked;
+  MyClockGen clock_gen (
+    .input_clk_25MHz(external_clk_25MHz),
+    .clk_proc(clk_proc),
+    .locked(clk_locked)
+  );
 
   //uart receiver signal
   logic [7:0] rx_data;
   logic rx_ready;
 
+  //uart2cpu signal
+  logic [7:0] data2cpu_uart;
+  logic [7:0] data2cpu_cpu;
+  assign data2cpu_uart = rx_ready ? rx_data : 8'h00;
+  assign led = data2cpu_cpu;
+
   //uart transmitter signal
   logic [7:0] tx_data;
   logic tx_ready; 
   logic tx_busy;
-  logic [$clog2(MAX_INPUT_WIDTH)-1:0] tx_index, tx_index_next;
+
+  //cpu2uart signal
+  logic [7:0] data2uart_cpu;
+  logic [7:0] data2uart_uart;
+  assign tx_ready = !tx_busy;
+  assign tx_data = data2uart_uart;
   
   rxuartlite uart_receive(
     .i_clk(external_clk_25MHz),
@@ -80,7 +101,50 @@ module SystemDemo(
     .o_wr(rx_ready),
     .o_data(rx_data)
   );
+    
+  DP16KD #(
+    .DATA_WIDTH_A(9), // Data width for port A
+    .DATA_WIDTH_B(9), // Data width for port B
+    .REGMODE_A("NOREG"), // Output register configuration for port A
+    .REGMODE_B("NOREG"), // Output register configuration for port B
+    .RESETMODE("SYNC"), // Reset mode
+    .ASYNC_RESET_RELEASE("SYNC"), // Asynchronous reset release mode
+    .WRITEMODE_A("NORMAL"), // Write mode for port A
+    .WRITEMODE_B("NORMAL")  // Write mode for port B
+  ) uart2cpu (
+    // Address for uart: use only the first entry
+    .ADA13(1'b0), .ADA12(1'b0), .ADA11(1'b0), .ADA10(1'b0), .ADA9(1'b0), .ADA8(1'b0), .ADA7(1'b0),
+    .ADA6(1'b0), .ADA5(1'b0), .ADA4(1'b0), .ADA3(1'b0), .ADA2(1'b0), .ADA1(1'b0), .ADA0(1'b0),
+    // Data from uart_Rx to CPU        
+    .DIA8(1'b0), .DIA7(data2cpu_uart[7]), .DIA6(data2cpu_uart[6]),   
+    .DIA5(data2cpu_uart[5]), .DIA4(data2cpu_uart[4]), .DIA3(data2cpu_uart[3]), 
+    .DIA2(data2cpu_uart[2]), .DIA1(data2cpu_uart[1]), .DIA0(data2cpu_uart[0]),
+    // Control signals for port A
+    .CEA(1'b1),         // Clock enable for port A
+    .OCEA(1'b1),        // Output clock enable for port A
+    .CLKA(external_clk_25MHz),       // Clock for port A
+    .WEA(rx_ready),         // Write enable for port A
+    .RSTA(1'b0),        // Reset for port A
 
+    // Address for port CPU read: same as uart
+    .ADB13(1'b0), .ADB12(1'b0), .ADB11(1'b0), .ADB10(1'b0), .ADB9(1'b0), .ADB8(1'b0), .ADB7(1'b0),
+    .ADB6(1'b0), .ADB5(1'b0), .ADB4(1'b0), .ADB3(1'b0), .ADB2(1'b0), .ADB1(1'b0), .ADB0(1'b0),
+    //To clear after read
+    .DIB8(1'b0), .DIB7(mem_data_to_write[7]), .DIB6(mem_data_to_write[6]),   
+    .DIB5(mem_data_to_write[5]), .DIB4(mem_data_to_write[4]), .DIB3(mem_data_to_write[3]), 
+    .DIB2(mem_data_to_write[2]), .DIB1(mem_data_to_write[1]), .DIB0(mem_data_to_write[0]),
+    // Data output for CPU read 
+    .DOB8(), .DOB7(data2cpu_cpu[7]), .DOB6(data2cpu_cpu[6]),   
+    .DOB5(data2cpu_cpu[5]), .DOB4(data2cpu_cpu[4]), .DOB3(data2cpu_cpu[3]), 
+    .DOB2(data2cpu_cpu[2]), .DOB1(data2cpu_cpu[1]), .DOB0(data2cpu_cpu[0]),
+    // Control signals for port B
+    .CEB(1'b1),         // Clock enable for port B
+    .OCEB(1'b1),        // Output clock enable for port B
+    .CLKB(clk_proc),       // Clock for port B
+    .WEB(mem_data_addr == MmapInput && |mem_data_we), // Write enable for port B
+    .RSTB(1'b0)//,        // Reset for port B
+  );
+  
   txuartlite uart_transmit(
     .i_clk(external_clk_25MHz),
     .i_reset(1'b0),
@@ -89,76 +153,81 @@ module SystemDemo(
     .o_uart_tx(ftdi_rxd),
     .o_busy(tx_busy)
   );
+  
+  DP16KD #(
+    .DATA_WIDTH_A(9), // Data width for port A
+    .DATA_WIDTH_B(9), // Data width for port B
+    .REGMODE_A("NOREG"), // Output register configuration for port A
+    .REGMODE_B("NOREG"), // Output register configuration for port B
+    .RESETMODE("SYNC"), // Reset mode
+    .ASYNC_RESET_RELEASE("SYNC"), // Asynchronous reset release mode
+    .WRITEMODE_A("NORMAL"), // Write mode for port A
+    .WRITEMODE_B("NORMAL")  // Write mode for port B
+  ) cpu2uart (
+    // Address for CPU: use only the first entry
+    .ADA13(1'b0), .ADA12(1'b0), .ADA11(1'b0), .ADA10(1'b0), .ADA9(1'b0), .ADA8(1'b0), .ADA7(1'b0),
+    .ADA6(1'b0), .ADA5(1'b0), .ADA4(1'b0), .ADA3(1'b0), .ADA2(1'b0), .ADA1(1'b0), .ADA0(1'b0),
+    // Data from CPU to uart_Tx        
+    .DIA8(1'b0), .DIA7(data2uart_cpu[7]), .DIA6(data2uart_cpu[6]),   
+    .DIA5(data2uart_cpu[5]), .DIA4(data2uart_cpu[4]), .DIA3(data2uart_cpu[3]), 
+    .DIA2(data2uart_cpu[2]), .DIA1(data2uart_cpu[1]), .DIA0(data2uart_cpu[0]),
+    // Control signals for port A
+    .CEA(1'b1),         // Clock enable for port A
+    .OCEA(1'b1),        // Output clock enable for port A
+    .CLKA(clk_proc),       // Clock for port A
+    .WEA(mem_data_addr == MmapOutput && |mem_data_we),  // Write enable for port A
+    .RSTA(1'b0),        // Reset for port A
 
-  //Have a pool to store all the input file
-  //When "enter" key is pressed, the input is completed
-  //When rx_ready is high, rx_dta will be registered
-  logic [7:0] pool [0:MAX_INPUT_WIDTH-1];
-  logic [7:0] pool_next [0:MAX_INPUT_WIDTH-1];
+    // Address for port Uart_Tx read
+    .ADB13(1'b0), .ADB12(1'b0), .ADB11(1'b0), .ADB10(1'b0), .ADB9(1'b0), .ADB8(1'b0), .ADB7(1'b0),
+    .ADB6(1'b0), .ADB5(1'b0), .ADB4(1'b0), .ADB3(1'b0), .ADB2(1'b0), .ADB1(1'b0), .ADB0(1'b0),
+    // Data output for Uart_Tx read 
+    .DOB8(), .DOB7(data2uart_uart[7]), .DOB6(data2uart_uart[6]),   
+    .DOB5(data2uart_uart[5]), .DOB4(data2uart_uart[4]), .DOB3(data2uart_uart[3]), 
+    .DOB2(data2uart_uart[2]), .DOB1(data2uart_uart[1]), .DOB0(data2uart_uart[0]),
+    // Control signals for port B
+    .CEB(1'b1),         // Clock enable for port B
+    .OCEB(1'b1),        // Output clock enable for port B
+    .CLKB(external_clk_25MHz),       // Clock for port B
+    .WEB(1'b0),         // Write enable for port B (disabled)
+    .RSTB(1'b0)//,        // Reset for port B
+  );
 
-  //len points to the next space to be written
-  logic [$clog2(MAX_INPUT_WIDTH)-1:0] len, len_next;
+  wire [31:0] pc_to_imem, insn_from_imem, mem_data_addr, mem_data_loaded_value, mem_data_to_write;
+  wire [3:0] mem_data_we;
+  wire [31:0] trace_writeback_pc, trace_writeback_insn;
+  cycle_status_e trace_writeback_cycle_status;
 
-  //When input_done is high, the input is completed
-  logic input_done, input_done_next;
+  assign data2uart_cpu = mem_data_to_write[7:0];
 
-  initial len = 0;
-  initial input_done = 0;
-  initial tx_index = 0;
-  initial for(int i = 0; i < MAX_INPUT_WIDTH; i = i + 1) begin
-    pool[i] = 8'h00;
-  end
+  MemorySingleCycle #(
+      .NUM_WORDS(1024)
+  ) memory (
+      .rst (!clk_locked),
+      .clk (clk_proc),
+      // imem is read-only
+      .pc_to_imem(pc_to_imem),
+      .insn_from_imem(insn_from_imem),
+      // dmem is read-write
+      .addr_to_dmem(mem_data_addr),
+      .load_data_from_dmem(mem_data_loaded_value),
+      .store_data_to_dmem (mem_data_to_write),
+      .store_we_to_dmem  (mem_data_addr == MmapOutput ? 4'd0 : mem_data_we)
+  );
 
-  always_comb begin
-    //Initial value
-    len_next = len;
-    input_done_next = input_done;
-    tx_index_next = tx_index;
-    tx_data = 8'h00;
-
-    for(int i = 0; i < MAX_INPUT_WIDTH; i = i + 1) begin
-      pool_next[i] = pool[i];
-    end
-
-    //Change value
-    //Input
-    if(rx_ready && input_done == 0) begin
-      pool_next[len] = rx_data;
-      if(rx_data == 8'h0D) begin
-        len_next = len;
-        input_done_next = 1'b1;
-      end
-      else begin
-        len_next = len + 1;
-      end
-    end
-    //Output
-    else if(tx_ready) begin
-      tx_index_next = tx_index + 1;
-      tx_data = pool[tx_index];
-      if(tx_index == len) begin
-        tx_index_next = 0;
-        input_done_next = 0;
-        len_next = 0;
-        for(int i = 0; i < MAX_INPUT_WIDTH; i = i + 1) begin
-          pool_next[i] = 8'h00;
-        end
-      end
-    end
-  end
-
-  always_ff@(posedge external_clk_25MHz) begin
-    len <= len_next;
-    input_done <= input_done_next;
-    tx_index <= tx_index_next;
-    for(int i = 0; i < MAX_INPUT_WIDTH; i = i + 1) begin
-      pool[i] <= pool_next[i];
-    end
-  end
-
-  //Might be buggy
-  assign led = {1'b0,len[2:0],3'b0,input_done};
-  assign tx_ready = input_done && !tx_busy;
-  assign wifi_gpio0 = 1'b1;
+  DatapathPipelined datapath (
+      .clk(clk_proc),
+      .rst(!clk_locked),
+      .pc_to_imem(pc_to_imem),
+      .insn_from_imem(insn_from_imem),
+      .addr_to_dmem(mem_data_addr),
+      .store_data_to_dmem(mem_data_to_write),
+      .store_we_to_dmem(mem_data_we),
+      .load_data_from_dmem(mem_data_addr == MmapInput ? {24'd0, data2cpu_cpu} : mem_data_loaded_value),
+      .halt(),
+      .trace_writeback_pc(trace_writeback_pc),
+      .trace_writeback_insn(trace_writeback_insn),
+      .trace_writeback_cycle_status(trace_writeback_cycle_status)
+  );
 
 endmodule
--- hw5-pipelined/uart-c/uart.c
+++ hw5-pipelined/uart-c/uart.c
@@ -1,5 +1,6 @@
 #define MAX_LEN 128
 
+//#include <string.h>
 //define struct for input & output memory region
 //typedef struct {
 //    volatile int len;
@@ -11,21 +12,21 @@
 //volatile region_t* const OUTPUT = (volatile region_t* const) 0xFF001000;
 
 // memory-mapped device addresses are volatile so the compiler doesn't register-allocate them
-volatile char* const OUTPUT = (volatile char* const) 0xFF002000;
-volatile char* const INPUT  = (volatile char* const) 0xFF001000;
+volatile char* const OUTPUT = (volatile char* const) 0xFF001000;
+volatile char* const INPUT  = (volatile char* const) 0xFF002000;
 
 /** Wait for approximately the desired numberof milliseconds, then return. */
-//void wait_for_millis(unsigned long millis) {
-//    const unsigned long freq_mhz = 4000000; // Adjust for actual clock frequency
-//    const unsigned long ns_per_sec = 1000000000;
-//    const unsigned long ns_per_clock = ns_per_sec / freq_mhz;
-//    // Note: there are 3 instructions (~3 cycles) per loop iteration
-//    const unsigned long ns_per_iteration = ns_per_clock * 3;
-//    const unsigned long delay_ns = millis * 1000000;
-//    for (unsigned long i = 0; i < (delay_ns / ns_per_iteration); i++) {
-//        __asm__ volatile ("nop");
-//    }
-//}
+void wait_for_millis(unsigned long millis) {
+    const unsigned long freq_mhz = 20000000; // Adjust for actual clock frequency
+    const unsigned long ns_per_sec = 1000000000;
+    const unsigned long ns_per_clock = ns_per_sec / freq_mhz;
+    // Note: there are 3 instructions (~3 cycles) per loop iteration
+    const unsigned long ns_per_iteration = ns_per_clock * 3;
+    const unsigned long delay_ns = millis * 1000000;
+    for (unsigned long i = 0; i < (delay_ns / ns_per_iteration); i++) {
+        __asm__ volatile ("nop");
+    }
+}
 
 // ensure _start() is at address 0x0, the datapath's starting PC
 void _start() __attribute__((section(".start"))) __attribute__((naked));
@@ -33,71 +34,47 @@ void _start() {
     // setup stack pointer sp. NB: stack grows down (to smaller addresses)
     asm volatile ("li sp, 0xFFFFFFFC");
 
-    char input_done = 'n';
-    char first_char = '\0';
-    int input_len = 0;
-    char input[128]; //input string
-
-    int output_len = 0;
-    char output[128]; //output string
-
-    //
+    //char input_done = 'n';
+    char new_char = 0;
+    char old_char = 0;
+    //int input_len = 0;
+    //int output_len = 0;
+    //char input[MAX_LEN]; //input string
+    //for(int i = 0; i < MAX_LEN; i++) {
+    //    input[i] = 0;
+    //}
 
     while (1) {
-        //Input is not done
-        if(input_done == 'n') {
-            first_char = *INPUT;
-            //You should modify the logic of uart_rx_data a little bit
-            //We need to set it back to '\0' after setting it to input
-            //Input is not done yet
-            if(first_char != '\0') {
-                input[input_len] = first_char;
-                if(first_char == '\n') {
-                    input_done = 'y'; //When "enter" key is detected, input is done
-                }
-                else {
-                    if(input_len < MAX_LEN - 1)
-                        input_len++;
-                    else if(input_len == MAX_LEN - 1) {
-                        input_done = 'y'; //If the input hit the threshold, just end the input
-                    }
-                }
-            }
-            else {
-                continue; //go back to the top of the loop and read again
+        old_char = new_char;
+        new_char = *INPUT;
+        if(old_char != new_char) { //New character posedge
+            *OUTPUT = new_char + 1;
+            for (unsigned long i = 0; i < 1000000; i++) {
+                __asm__ volatile ("nop");
             }
         }
-        //Input is done
-        else if(input_done == 'y') {
-            //--------MODIFICATION OF THE INPUT DATA--------//
-            output_len = input_len;
-            for(int i = 0; i < output_len; i++) {
-                output[i] = input[output_len - 1 - i]; //Reverse the input string
-            }
-
-            //Just to be safe, erase the value in the input array & input_len
-            for(int i = 0; i < input_len; i++) {
-                input[i] = '\0';
-            }
-            input_len = 0;
-
-            //--------SEND THE MODIFIED DATA TO OUTPUT--------//
-            int index = 0;
-            while(1) {
-                //Hardware needs to set "\0"
-                if(*OUTPUT == '\0' && index < output_len) {
-                    *OUTPUT = output[index];
-                    index++;
-                }
-                else if(index == output_len) {
-                    output_len = 0;
-                    input_done = 'n';
-                    for(int i = 0; i < output_len; i++) {
-                        output[i] = '\0';
-                    }
-                    break;
-                }
-            }
+        else {
+            *OUTPUT = 0;
         }
+
+        //if(new_char != 0 && new_char != '\n' && input_len < MAX_LEN) { //Input is not done
+        //    wait_for_millis(100);
+        //    *INPUT = 0;
+        //    input[input_len] = new_char;
+        //    input_len++;
+        //    continue;
+        //}
+        //if(new_char == '\n' && output_len < input_len) { //Input is done
+        //    *OUTPUT = input[output_len];
+        //    output_len++;
+        //    continue;
+        //}   
+        //if(new_char == '\n' && output_len == input_len) { //Output is done
+        //    *OUTPUT = '\n';
+        //    *INPUT = 0;
+        //    input_len = 0;
+        //    output_len = 0;
+        //    continue;
+        //}
     } 
 }
