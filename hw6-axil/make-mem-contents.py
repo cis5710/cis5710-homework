34bbf61d917db82d438e606fd555f66446d26e29
Joe Devietti <devietti@cis.upenn.edu>
2026-04-28 11:00:34 -0400

    include steps to build atarvi-native games

--- hw6-axil/make-mem-contents.py
+++ hw6-axil/make-mem-contents.py
@@ -6,7 +6,9 @@ sys.path.append(str(p))
 import riscv_binary_utils
 
 riscv_binary_utils.loadBinaryIntoHexFile(
-    'atarvi-native/target/riscv32im-unknown-none-elf/release/candycrvsh',
+    sys.argv[1],    
     'mem_initial_contents.hex',
     maxAddress=8192*4
+    #maxAddress=4096*4
+    #maxAddress=512*4
 )
